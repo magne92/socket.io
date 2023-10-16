70c7323f164594c55c74e8a6873fc56c9df78e61
magne92 <92579836+magne92@users.noreply.github.com>
2023-10-16 10:58:07 +0200

    socketio
    board update, click events

--- socket_server.js
+++ socket_server.js
@@ -6,13 +6,49 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+const users = {}
 
+var turn = 1
+
+var board = [
+        [" ", " ", " "],
+        [" ", " ", " "],
+        [" ", " ", " "]
+    ]
 
 io.on('connection', (socket) => {
     console.log('A user connected');
+    io.emit('update_board', board);
 
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg); // Broadcast the message to all clients
+
+    socket.on('add_mark', (msg) => {
+
+        console.log(msg)
+        var row = Math.floor(parseInt(msg) / 3)
+        var col = parseInt(msg) % 3
+
+        if (row > 2) {
+            row = 2
+            console.log("row was above 2")
+        }
+
+        console.log(row,col)
+
+        //var row = msg[0]
+        //var col = msg[1]
+        if (msg) {
+            if (board[row][col] == " ") {
+                if (turn % 2 == 0){
+                    board[row][col] = "O"
+                } else {
+                    board[row][col] = "X"
+                }
+                turn++
+        
+                console.log(board)
+                io.emit('update_board', board); // Broadcast the message to all clients
+            }
+        }
     });
 
     socket.on('disconnect', () => {

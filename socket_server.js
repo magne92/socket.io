da368c065e8d9b9b321affc008db0bb0aca72624
magne92 <92579836+magne92@users.noreply.github.com>
2023-10-22 18:29:08 +0200

    update

--- socket_server.js
+++ socket_server.js
@@ -6,53 +6,92 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-const users = {}
+var users = []
 
-var turn = 1
+var turns_taken = 0
 
-var board = [
-        [" ", " ", " "],
-        [" ", " ", " "],
-        [" ", " ", " "]
-    ]
+var turns = ["X", "O"]
+var active_turn
+var board
 
+function new_round() {
+    board = [
+            [" ", " ", " "],
+            [" ", " ", " "],
+            [" ", " ", " "]
+        ]
+    active_turn = "X"
+    turns_taken = 0
+}
+
+new_round()
+    
 io.on('connection', (socket) => {
     console.log('A user connected');
-    io.emit('update_board', board);
+    io.emit('update_board', board, active_turn);
     console.log("test of restart")
 
+    console.log("user length:", users.length)
+
+    if (users.length == 0) {
+        users.push([socket.id, "X"])
+    } else if (users.length == 1) {
+        users.push([socket.id, "O"])
+    } 
+
+    if (users[0][0] == socket.id) {
+        io.to(socket.id).emit("player_select", users[0][1]); 
+    }
+    else if (users[1][0] == socket.id) {
+        io.to(socket.id).emit("player_select", users[1][1]); 
+    } else {
+        io.to(socket.id).emit("player_select", "Spectator");
+    } 
+
     socket.on('add_mark', (msg) => {
-
-        console.log(msg)
-        var row = Math.floor(parseInt(msg) / 3)
-        var col = parseInt(msg) % 3
-
-        if (row > 2) {
-            row = 2
-            console.log("row was above 2")
+        not_active_player = false
+        if (socket.id == users[0][0]){
+            turn_symbol = "X"
+        } else if (socket.id == users[1][0]) {
+            turn_symbol = "O"
+        } else {
+            not_active_player = true
+            console.log("non player tried to add mark")
         }
 
-        console.log(row,col)
+        if (!not_active_player) {
+            console.log(msg)
+            var row = Math.floor(parseInt(msg) / 3)
+            var col = parseInt(msg) % 3
 
-        //var row = msg[0]
-        //var col = msg[1]
-        if (msg) {
-            if (board[row][col] == " ") {
-                if (turn % 2 == 0){
-                    board[row][col] = "O"
-                } else {
-                    board[row][col] = "X"
+            console.log(row,col)
+    
+            if (msg && turn_symbol == active_turn) {
+                if (board[row][col] == " ") {      
+                    if (active_turn == "X"){
+                        board[row][col] = "X"
+                        active_turn = "O"
+                    } else {
+                        board[row][col] = "O"
+                        active_turn = "X"
+                    }
+                    console.log(board)
+                    turns_taken++
+                    io.emit('update_board', board, active_turn); // Broadcast the message to all clients
                 }
-                turn++
-        
-                console.log(board)
-                io.emit('update_board', board); // Broadcast the message to all clients
             }
         }
     });
 
     socket.on('disconnect', () => {
+        if (socket.id == users[0][0]) {
+            users.splice(0,1)
+        } else if (socket.id == users[1][0] ){
+            users.pop()
+        }
         console.log('A user disconnected');
+        console.log("users: ", users.length)
+        new_round()
     });
 });
 
